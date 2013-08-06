2c054adf04528435731bc17ad50b15448e6ac793
Gehan Gonsalkorale <gehan.g@gmail.com>
2013-08-06 17:27:04 +0000

    Make some basic models

---
+++ backend/seed/fixtures/test_data.py
@@ -0,0 +1,6 @@
+from seed.documents import Message
+
+Message.objects.create(text='hello')
+Message.objects.create(text='yeah mate')
+Message.objects.create(text='whats going on')
+Message.objects.create(text='nice one')
