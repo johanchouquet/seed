c04f958aa004f0e87e993fbc445be20b4399aec9
Gehan Gonsalkorale <gehan.g@gmail.com>
2013-10-09 21:51:32 +0000

    Add basic layout

--- app/spec.js
+++ app/spec.js
@@ -1,3 +1,26 @@
 define({
+    // Normalise browser environment using normalize.css
+    reset: {
+        module: 'css!lib/normalize-css/normalize.css'
+    },
+
+    // Create basic layout
+    layout: {
+        render: {
+            template: {
+                module: 'text!app/layout.html'
+            },
+            css: {
+                module: 'css!app/layout.css'
+            }
+        },
+        insert: {
+            at: {
+                $ref: 'dom.first!body'
+            }
+        }
+    },
+
+    plugins: ['wire/dom', 'wire/dom/render']
 
 });
