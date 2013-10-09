5df226c4d21d3c2a6ba6ffad3e290e8387c6b2e5
Gehan Gonsalkorale <gehan.g@gmail.com>
2013-10-09 23:23:43 +0000

    Start creating other views

--- app/spec.js
+++ app/spec.js
@@ -21,6 +21,60 @@ define({
         }
     },
 
+    // Top menu view
+    topMenuView: {
+        render: {
+            template: {
+                module: 'text!app/top-menu.html'
+            },
+            css: {
+                module: 'css!app/top-menu.css'
+            }
+        },
+        insert: {
+            at: {
+                $ref: 'dom.first!.top-menu',
+                at: 'layout'
+            }
+        }
+    },
+
+    // Left nav view
+    leftNavView: {
+        render: {
+            template: {
+                module: 'text!app/left-nav.html'
+            },
+            css: {
+                module: 'css!app/left-nav.css'
+            }
+        },
+        insert: {
+            at: {
+                $ref: 'dom.first!.left-nav',
+                at: 'layout'
+            }
+        }
+    },
+
+    // Main body view
+    mainContentView: {
+        render: {
+            template: {
+                module: 'text!app/main-content.html'
+            },
+            css: {
+                module: 'css!app/main-content.css'
+            }
+        },
+        insert: {
+            at: {
+                $ref: 'dom.first!.main-content',
+                at: 'layout'
+            }
+        }
+    },
+
     plugins: ['wire/dom', 'wire/dom/render']
 
 });
